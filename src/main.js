d47e4597be388f0fcc0e216a8efe05a111381f0d
Ian Rodrigues <ian.rodrigues.knight@gmail.com>
2026-08-28 17:16:23 -0500

    Improved caching and downloaded fonts

--- src/main.js
+++ src/main.js
@@ -542,3 +542,9 @@ function queueNextUpdate() {
 
 updateEverything();
 queueNextUpdate();
+
+if (import.meta.env.PROD && "serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {});
+  });
+}
